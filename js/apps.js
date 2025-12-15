d2dfecafab105731b66359383ec3b04ff3da2549
Bryant <114641124+bryantchendra@users.noreply.github.com>
2025-12-15 16:08:53 +0700

    Create apps.js

---
+++ js/apps.js
@@ -0,0 +1,6 @@
+// js/app.js
+
+// Load the particles.json config and attach it to the #particles-js element
+particlesJS.load('particles-js', 'assets/particles.json', function () {
+  console.log('particles.js config loaded');
+});
